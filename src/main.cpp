cd29b98d0dac97db7dbd68e5e4431a1101e64a0f
Erthium <ertyum35@gmail.com>
2024-04-05 17:27:29 +0300

    feat: correctly use everything in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include "ascii_conv.hpp"
+#include "converter.hpp"
 
 using namespace std;
 
@@ -10,16 +10,18 @@ SDL_Texture* texture = IMG_LoadTexture(renderer, path.c_str());
 
 // images to convert
 #define PATH_1 "assets/images/spider.png"
+#define FONT_PATH "assets/fonts/mono_regular.ttf" 
 
 // density tables
-static string DENSITY_TABLE_1 = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
-static string DENSITY_TABLE_2 = " .:-=+*#%@";
+const string DENSITY_TABLE_1 = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'.                                                                                                                                                      ";
+const string DENSITY_TABLE_2 = " .:-=+*#%@";
 
 
 ASCII_Converter *converter = nullptr;
 int main(){
-    converter = new ASCII_Converter(DENSITY_TABLE_1, PATH_1);
-    converter->show();
+    converter = new ASCII_Converter(DENSITY_TABLE_2, TEST_IMAGE_PATH, FONT_PATH);
+    converter->convert(8, true);
+    converter->loop();
     delete converter;
     return 0;
 }
